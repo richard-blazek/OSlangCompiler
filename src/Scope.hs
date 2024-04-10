14da6f89e9745220a8f7047de59876ae06937ec2
Richard Blažek <blazek.risa@gmail.com>
2024-04-10 12:39:48 +0100

    Briefer code

--- src/Scope.hs
+++ src/Scope.hs
@@ -1,9 +1,9 @@
 module Scope (Scope, empty, include, get, addPublic, addPrivate) where
 
 import qualified Data.Map.Strict as Map
-import qualified Language as Lang
+import Language (Type)
 
-data Entry = Public Lang.Type | Private Lang.Type deriving (Show, Eq, Ord)
+data Entry = Public Type | Private Type deriving (Show, Eq, Ord)
 data Scope = Scope (Map.Map String Entry) deriving (Show, Eq, Ord)
 
 isPublic :: Entry -> Bool
@@ -21,14 +21,14 @@ include (Scope other) (Scope this) = Scope union
   where added = Map.filter isPublic other
         union = Map.unionWith nameConflict added this
 
-get :: String -> Scope -> Lang.Type
+get :: String -> Scope -> Type
 get name (Scope this) = case Map.lookup name this of
   Just (Public t) -> t
   Just (Private t) -> t
   Nothing -> error "Unknown name"
 
-addPublic :: String -> Lang.Type -> Scope -> Scope
+addPublic :: String -> Type -> Scope -> Scope
 addPublic name t (Scope this) = Scope (Map.insertWith nameConflict name (Public t) this)
 
-addPrivate :: String -> Lang.Type -> Scope -> Scope
+addPrivate :: String -> Type -> Scope -> Scope
 addPrivate name t (Scope this) = Scope (Map.insertWith nameConflict name (Private t) this)
