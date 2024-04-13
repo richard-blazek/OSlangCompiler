47fde5b6d5d4318a082f8f5d3c34ee20e28444c7
Richard Blažek <blazek.risa@gmail.com>
2024-04-13 22:38:32 +0100

    Better way to handle builtins

--- src/Typing.hs
+++ src/Typing.hs
@@ -1,6 +1,6 @@
-module Typing (Type (..), isInstantiable, isBuiltin, access, value, function, operator) where
+module Typing (Type (..), isInstantiable, access, globals, operator) where
 
-import Data.Map.Strict (Map, (!))
+import Data.Map.Strict (Map, (!), fromList)
 
 data Type
   = Void
@@ -12,48 +12,31 @@ data Type
   | Pointer Type
   | Function [Type] Type
   | Record String (Map String Type)
-  | Type Type deriving (Show, Eq, Ord)
+  | Type Type
+  | Builtin String deriving (Show, Eq, Ord)
 
 isInstantiable :: Type -> Bool
 isInstantiable (Type t) = False
 isInstantiable t = t /= Void
 
-isBuiltin :: String -> Bool
-isBuiltin = (`elem` ["Void", "Bool", "I8", "I16", "I32", "I64", "Ptr", "Fun"])
-
 access :: String -> Type -> Type
 access name (Record _ fields) = fields ! name
 access name (Pointer (Record _ fields)) = Pointer (fields ! name)
 
-value :: String -> Type
-value "Void" = Type Void
-value "Bool" = Type Bool
-value "I8" = Type Int8
-value "I16" = Type Int16
-value "I32" = Type Int32
-value "I64" = Type Int64
-
-isInteger :: Type -> Bool
-isInteger = (`elem` [Int8, Int16, Int32, Int64])
-
-isLogical :: Type -> Bool
-isLogical = (`elem` [Int8, Int16, Int32, Int64, Bool])
-
-isIntegral :: Type -> Bool
-isIntegral (Pointer _) = True
-isIntegral t = isLogical t
-
-function :: String -> [Type] -> Type
-function fn args = case (fn, args) of
-  ("Fun", args) -> let types = map (\(Type t) -> t) args in Type (Function (init args) (last args))
-  ("Ptr", [Type t]) -> Type (Pointer t)
-  ("cast", [Type t, u]) | isIntegral t && isIntegral u -> t
+globals :: Map String Type
+globals = fromList [
+  ("Void", Type Void), ("Bool", Type Bool),
+  ("I8", Type Int8), ("I16", Type Int16), ("I32", Type Int32), ("I64", Type Int64),
+  ("Fun", Builtin "Fun"), ("Ptr", Builtin "Ptr"), ("cast", Builtin "cast")]
 
 operator :: String -> [Type] -> Type
 operator op args = case (op, args) of
   ("if", [Bool, t, u]) -> if t == u then t else Void
   ("while", [Bool, t]) -> Void
   ("[]", (Function args result : rest)) | args == rest -> result
+  ("[]", (Builtin "Fun" : args)) -> let types = map (\(Type t) -> t) args in Type (Function (init types) (last types))
+  ("[]", [Builtin "Ptr", Type t]) -> Type (Pointer t)
+  ("[]", [Builtin "cast", Type t, u]) | isIntegral t && isIntegral u -> t
   ("$", [Pointer t]) -> t
   ("~", [x]) | isInteger x -> x
   ("!", [x]) | isLogical x -> x
@@ -77,3 +60,8 @@ operator op args = case (op, args) of
   ("<=", [t, u]) | t == u && isIntegral t -> Bool
   (">=", [t, u]) | t == u && isIntegral t -> Bool
   ("<-", [Pointer t, u]) | t == u -> Void
+
+  where isInteger = (`elem` [Int8, Int16, Int32, Int64])
+        isLogical = (`elem` [Int8, Int16, Int32, Int64, Bool])
+        isIntegral (Pointer _) = True
+        isIntegral t = isLogical t
